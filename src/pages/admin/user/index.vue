377a0d2a88bf4a5a191f994bdee0ed6da6107bce
yoyan1 <yoyan947@gmail.com>
2025-01-21 22:28:12 -0600

    refactor: remove placeholder text from transactions and residents pages

--- src/pages/admin/user/index.vue
+++ src/pages/admin/user/index.vue
@@ -68,7 +68,6 @@ const getStatusLabel = (status) => {
     <div class="bg-surface-50 dark:bg-surface-950 p-5">
         <div class="bg-surface-0 dark:bg-surface-900 p-6 shadow rounded-border">
             <div class="text-3xl font-medium text-surface-900 dark:text-surface-0 mb-2">Residents</div>
-            <div class="font-medium text-surface-500 dark:text-surface-300 mb-4">Vivamus id nisl interdum, blandit augue sit amet, eleifend mi.</div>
             <div>
                 <div>
                     <Toolbar class="mb-6">
