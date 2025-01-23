5749b3a8ae3c9311b7e0cfd603feaebad11bd585
5kydriven <tagpuno619@gmail.com>
2025-01-23 10:06:43 +0800

    Refactor documentation and improve admin layout with routing; remove unused type definitions

--- src/layouts/admin-layout.vue
+++ src/layouts/admin-layout.vue
@@ -1,44 +1,50 @@
 <script setup lang="ts">
-	import { useRoute } from 'vue-router';
+	import { useRoute, RouterLink } from 'vue-router';
 
 	const route = useRoute();
 </script>
 
 <template>
 	<main class="flex h-screen w-full">
-		<aside class="w-64 h-full transition-transform border-r shadow-lg">
+		<aside
+			class="w-64 h-full transition-transform border-r shadow-lg hidden lg:block">
 			<div
 				class="h-full p-4 overflow-y-auto flex flex-col bg-gray-50 dark:bg-gray-800">
 				<a class="flex items-center ps-2.5 mb-5">
 					<span
 						class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-						>LOGO/TITLE</span
+						>CWSs</span
 					>
 				</a>
 				<ul class="space-y-2 font-medium grow">
 					<li>
-						<a
-							href="/admin"
-							:class="route.name == 'admin' ? 'bg-primary-500' : ''"
-							class="flex items-center p-2 rounded-lg text-white cursor-pointer hover:bg-primary-400">
-							<span>Dashboard</span>
-						</a>
+						<RouterLink to="/admin">
+							<a
+								:class="route.name == 'admin' ? 'bg-primary-500' : ''"
+								class="flex items-center p-2 rounded-lg text-black hover:text-white cursor-pointer hover:bg-primary-400">
+								<span>Dashboard</span>
+							</a>
+						</RouterLink>
 					</li>
 					<li>
-						<a
-							href="/admin/user"
-							:class="route.name == 'admin-user' ? 'bg-primary-500' : ''"
-							class="flex items-center p-2 text-gray-900 rounded-lg hover:text-white hover:bg-primary-400 dark:text-white cursor-pointer">
-							<span class="flex-1 whitespace-nowrap">Users</span>
-						</a>
+						<RouterLink to="/admin/user">
+							<a
+								:class="route.name == 'admin-user' ? 'bg-primary-500' : ''"
+								class="flex items-center p-2 text-gray-900 rounded-lg hover:text-white hover:bg-primary-400 dark:text-white cursor-pointer">
+								<span class="flex-1 whitespace-nowrap">Users</span>
+							</a>
+						</RouterLink>
 					</li>
 					<li>
-						<a
-							href="/admin/transaction"
-							:class="route.name == 'admin-transaction' ? 'bg-primary-500' : ''"
-							class="flex items-center p-2 text-gray-900 rounded-lg hover:text-white dark:text-white hover:bg-primary-400 cursor-pointer">
-							<span class="flex-1 whitespace-nowrap">Transactions</span>
-						</a>
+						<RouterLink to="/admin/transaction">
+							<a
+								:class="
+									route.name == 'admin-transaction' ? 'bg-primary-500' : ''
+								"
+								class="flex items-center p-2 text-gray-900 rounded-lg hover:text-white dark:text-white hover:bg-primary-400 cursor-pointer">
+								<span class="flex-1 whitespace-nowrap">Transactions</span>
+							</a>
+						</RouterLink>
 					</li>
 				</ul>
 				<Button label="Sign Out" />
