c8b7c34093d7ea178e68a590c2c742b8f8ea0483
5kydriven <tagpuno619@gmail.com>
2025-02-07 21:50:42 +0800

    refactor: remove unused PrimeVue components and update main.ts for component registration

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import checker from 'vite-plugin-checker';
-import Components from 'unplugin-vue-components/vite';
-import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
-		Components({
-			resolvers: [PrimeVueResolver()],
-		}),
 		vueDevTools(),
 		checker({
 			vueTsc: true,
