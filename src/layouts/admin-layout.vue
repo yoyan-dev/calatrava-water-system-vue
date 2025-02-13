ec68298dd74a3c7c26ed1b1aca64ddf13afd756a
yoyan1 <yoyan947@gmail.com>
2025-02-13 19:12:33 +0800

    feat: add NavBar component to admin layout and adjust styles for improved navigation

--- src/layouts/admin-layout.vue
+++ src/layouts/admin-layout.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 	import { useRoute, RouterLink } from 'vue-router';
+	import NavBar from '@/components/navBar/index.vue'
 
 	const route = useRoute();
 </script>
@@ -11,26 +12,25 @@
 			class="w-64 h-full transition-transform border-r  hidden lg:block">
 			<div
 				class="h-full p-4 overflow-y-auto flex flex-col bg-white dark:bg-gray-800">
-				<a class="flex items-center ps-2.5 mb-5">
+				<a class="flex items-center justify-center ps-2.5 mb-3">
 					<span
 						class="self-center flex items-center text-2xl font-semibold whitespace-nowrap dark:text-white"
 						><Avatar
 							image="/logo.jpeg"
 							class="mr-2"
-							size="large"
-							shape="circle" />C<span class="text-primary text-lg">W</span
-						>S</span
-					>
-				</a>
+							size="xlarge"
+							shape="circle" />
+					</span>
+				</a><hr>
 				<ul class="space-y-2 font-medium grow">
 					<li>
 						<RouterLink to="/admin/dashboard" 
 						:class="
 							route.name == 'admin-dashboard'
-								? 'bg-primary-500 text-white'
+								? 'bg-primary-100 text-primary'
 								: ''
 						"
-						class="flex items-center px-2 rounded-lg text-black hover:text-white cursor-pointer hover:bg-primary-400">
+						class="flex items-center px-2 rounded-lg font-normal hover:text-white cursor-pointer hover:bg-primary-400">
 							<i class="pi pi-chart-bar"></i>
 							<a
 								class="flex items-center p-2 rounded-lg hover:text-white cursor-pointer hover:bg-primary-400">
@@ -42,10 +42,10 @@
 						<RouterLink to="/admin/users" 
 						:class="
 							route.name == 'admin-users' || route.name == 'admin-user'
-								? 'bg-primary-500 text-white'
+								? 'bg-primary-100 text-primary'
 								: ''
 						"
-						class="flex items-center px-2 rounded-lg text-black hover:text-white cursor-pointer hover:bg-primary-400">
+						class="flex items-center px-2 rounded-lg font-normal hover:text-white cursor-pointer hover:bg-primary-400">
 							<i class="pi pi-user"></i>
 							<a
 								class="flex items-center p-2 rounded-lg hover:text-white cursor-pointer hover:bg-primary-400">
@@ -57,10 +57,10 @@
 						<RouterLink to="/admin/readers" 
 						:class="
 							route.name == 'admin-readers' || route.name == 'admin-reader'
-								? 'bg-primary-500 text-white'
+								? 'bg-primary-100 text-primary'
 								: ''
 						"
-						class="flex items-center px-2 rounded-lg text-black hover:text-white cursor-pointer hover:bg-primary-400">
+						class="flex items-center px-2 rounded-lg font-normal hover:text-white cursor-pointer hover:bg-primary-400">
 							<i class="pi pi-users"></i>
 							<a
 								class="flex items-center p-2 rounded-lg hover:text-white cursor-pointer hover:bg-primary-400">
@@ -75,10 +75,10 @@
 								route.name == 'admin-billing' ||
 								route.name == 'admin-billing-create' ||
 								route.name == 'admin-billing-update'
-									? 'bg-primary-500 text-white'
+									? 'bg-primary-100 text-primary'
 									: ''
 							"
-						class="flex items-center px-2 rounded-lg text-black hover:text-white cursor-pointer hover:bg-primary-400">
+						class="flex items-center px-2 rounded-lg font-normal hover:text-white cursor-pointer hover:bg-primary-400">
 							<i class="pi pi-book"></i>
 							<a
 								class="flex items-center p-2 rounded-lg hover:text-white cursor-pointer hover:bg-primary-400">
@@ -91,9 +91,11 @@
 			</div>
 		</aside>
 		<div
-			class="w-full flex flex-col gap-5 bg-gray-50 p-5 overflow-auto">
-
-			<slot />
+			class="w-full flex flex-col gap-5 bg-gray-50 overflow-auto">
+			<NavBar/>
+			<div class="px-3">
+				<slot />
+			</div>
 		</div>
 	</main>
 </template>
