65dc9b462bef57b37747c940bae7d55113344fb5
yoyan1 <yoyan947@gmail.com>
2025-02-25 22:24:42 +0800

    refactor: add CreateReminderModal component and integrate it into billing header

---
+++ src/pages/admin/billing/_components/modals/create-reminder-modal.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const visible = ref(false);
+const reminder = ref({
+  message: "",
+});
+
+function submit() {}
+</script>
+<template>
+  <Button
+    severity="primary"
+    label="Send Reminder"
+    size="small"
+    @click="visible = true"
+  />
+  <Dialog
+    v-model:visible="visible"
+    modal
+    header="Send Reminder"
+    :style="{ width: '50rem' }"
+    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+  >
+    <form @submit.prevent="submit">
+      <div>
+        <Textarea
+          v-model="reminder.message"
+          rows="5"
+          class="bg-gray-100 w-full"
+          placeholder="Enter your content here..."
+        />
+      </div>
+      <div class="flex justify-end mt-2">
+        <Button
+          label="Send"
+          severity="primary"
+          icon="pi pi-send"
+          iconPos="right"
+          type="submit"
+        />
+      </div>
+    </form>
+  </Dialog>
+</template>
