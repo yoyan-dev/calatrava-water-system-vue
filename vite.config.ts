3cef08b8ed2817479cf9d16c365650e440f58cb1
5kydriven <tagpuno619@gmail.com>
2025-03-02 22:05:47 +0800

    refactor: update service worker registration type, enhance PWA configuration, and improve asset handling

--- vite.config.ts
+++ vite.config.ts
@@ -15,44 +15,49 @@ export default defineConfig(({ mode }) => {
 			checker({
 				vueTsc: true,
 			}),
-			// VitePWA({
-			// 	registerType: 'autoUpdate',
-			// 	devOptions: {
-			// 		enabled: true,
-			// 		type: 'module',
-			// 	},
-			// 	includeAssets: ['favicon.ico'],
-			// 	manifest: {
-			// 		name: 'My Awesome App',
-			// 		short_name: 'MyApp',
-			// 		description: 'My Awesome App description',
-			// 		theme_color: '#ffffff',
-			// 		icons: [
-			// 			{
-			// 				src: 'pwa-192x192.png',
-			// 				sizes: '192x192',
-			// 				type: 'image/png',
-			// 			},
-			// 			{
-			// 				src: 'pwa-512x512.png',
-			// 				sizes: '512x512',
-			// 				type: 'image/png',
-			// 			},
-			// 			{
-			// 				src: 'pwa-512x512.png',
-			// 				sizes: '512x512',
-			// 				type: 'image/png',
-			// 				purpose: 'any',
-			// 			},
-			// 			{
-			// 				src: 'pwa-512x512.png',
-			// 				sizes: '512x512',
-			// 				type: 'image/png',
-			// 				purpose: 'maskable',
-			// 			},
-			// 		],
-			// 	},
-			// }),
+			VitePWA({
+				registerType: 'autoUpdate',
+				strategies: 'injectManifest',
+				injectManifest: {
+					globPatterns: ['**/*.{js,css,html,png,ico,jpg,svg}'],
+				},
+				srcDir: 'public',
+				filename: 'firebase-messaging-sw.js',
+				devOptions: {
+					enabled: true,
+				},
+				includeAssets: ['favicon.ico'],
+				manifest: {
+					name: 'My Awesome App',
+					short_name: 'MyApp',
+					description: 'My Awesome App description',
+					theme_color: '#ffffff',
+					icons: [
+						{
+							src: 'pwa-192x192.png',
+							sizes: '192x192',
+							type: 'image/png',
+						},
+						{
+							src: 'pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+						},
+						{
+							src: 'pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'any',
+						},
+						{
+							src: 'pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'maskable',
+						},
+					],
+				},
+			}),
 		],
 		resolve: {
 			alias: {
