303b51241326abda44d1301bfe5dbd0681b63dea
5kydriven <tagpuno619@gmail.com>
2025-03-21 14:54:19 +0800

    refactor: update billing type definition and fix TODO status in documentation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-706c6701'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.qmlnpbdfvg"
+    "revision": "0.aphvc9m7f9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
