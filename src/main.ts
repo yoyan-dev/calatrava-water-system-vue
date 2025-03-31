5c397b016c8b132b1dd9a762bb7d2743b33323e9
yoyan1 <yoyan947@gmail.com>
2025-03-31 09:47:13 +0800

    feat: enable dynamic Firebase configuration in service worker and update service worker registration

--- src/main.ts
+++ src/main.ts
@@ -128,6 +128,17 @@ if ("serviceWorker" in navigator) {
     .register("/firebase-messaging-sw.js")
     .then((registration) => {
       console.log("✅ Service Worker registered successfully!", registration);
+      registration.active?.postMessage({
+        type: "SET_FIREBASE_CONFIG",
+        firebaseConfig: {
+          apiKey: import.meta.env.VITE_API_KEY,
+          authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+          projectId: import.meta.env.VITE_PROJECT_ID,
+          storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+          messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+          appId: import.meta.env.VITE_APP_ID,
+        },
+      });
     })
     .catch((error) => {
       console.error("Service Worker registration failed:", error);
--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(() => {
         injectRegister: "auto",
         devOptions: {
           enabled: false,
+          type: "module",
         },
         includeAssets: ["favicon.ico"],
         manifest: {
