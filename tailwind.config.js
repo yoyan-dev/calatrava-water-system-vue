0cc26791f27616277470b10c0e923e98794c4cc3
5kydriven <tagpuno619@gmail.com>
2025-04-06 20:13:30 +0800

    fix: standardize formatting in HTML and TypeScript files for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {},
-  plugins: [require("tailwindcss-primeui")],
+	darkMode: ['selector', '[class~="my-app-dark"]'],
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {},
+	plugins: [require('tailwindcss-primeui')],
 };
